c6bb6c9de3db511ccb9da71596eaff5c7626253e
saint <saintmoses100@gmail.com>
2022-06-25 17:14:16 -0300

    added mongo DB

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const debug = require('debug')
 const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+
+mongoose.connect("mongodb+srv://iknowsaint:Jajabone@e-commerce.jyo2lc6.mongodb.net/e-commerce?retryWrites=true&w=majority")
+ .then(()=> console.log("DB connection successful"))
+ .catch((err)=>{
+    console.log(err);
+ })
 
 const authRouter = require('./routes/auth.routes')
 const adminRouter =require('./routes/admin.routes')
